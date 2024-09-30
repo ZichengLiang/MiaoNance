d7d21f34398b7b2c0214026b09c54e955c6bc912
Zicheng_Liang <liangzicheng02@gmail.com>
2024-09-30 11:40:58 +0100

    Applied modular design for future expansions.(CLI arguments)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,17 +9,21 @@ import com.google.gson.JsonArray;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-        Map<String, String> allSymbols = new TreeMap<>();
-        Scanner symbols = new Scanner(new File("symbols.in"));
-        while (symbols.hasNextLine()) {
-            allSymbols.put(symbols.next(), "1M");
-        }
+        switch (args[0]) {
+            case "trend":
+                Map<String, String> allSymbols = new TreeMap<>();
+                Scanner symbols = new Scanner(new File("symbols.in"));
+                while (symbols.hasNextLine()) {
+                    allSymbols.put(symbols.next(), "1M");
+                }
 
-        for (String key : allSymbols.keySet()) {
-            Biance_SPOT spot = new Biance_SPOT(key, allSymbols.get(key));
-            JsonArray trend = spot.getResultAsJson();
-            XYLineChart theChart = new XYLineChart("小猫比特", key + "_by_" + allSymbols.get(key), key, trend);
-            theChart.exportAsPNG();
+                for (String key : allSymbols.keySet()) {
+                    Biance_SPOT spot = new Biance_SPOT(key, allSymbols.get(key));
+                    JsonArray trend = spot.getResultAsJson();
+                    XYLineChart theChart = new XYLineChart("小猫比特", key + "_by_" + allSymbols.get(key), key, trend);
+                    theChart.exportAsPNG();
+                }
+                break;
         }
     }
 }
