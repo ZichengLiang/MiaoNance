8b1528520aeadeb888ff1a0493b5b4ba677c30c6
dev-j03l <jojanj@tcd.ie>
2025-05-18 09:16:06 +0100

    Added a title onto the eye to make it more intuitive

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -69,6 +69,7 @@ export default function LoginPage() {
               type="button"
               onClick={() => setShowPassword((prev) => !prev)}
               className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500"
+              title={showPassword ? 'Hide password' : 'Show password'}
             >
               {showPassword ? <EyeOff size={18} /> : <Eye size={18} />}
             </button>
