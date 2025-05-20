dd7f31ecd50286b9067387f37d30fd2172602645
Zicheng Liang <liangzicheng02@gmail.com>
2025-05-20 21:52:59 +0800

    Prepare for a pop up card.

---
+++ frontend/src/app/vault/PopupCard.tsx
@@ -0,0 +1,18 @@
+import React, { JSXElementConstructor } from "react";
+
+interface PopupCardProps {
+    
+    variant:'delete' | 'edit'
+}
+
+function handleDeleteCard() {
+
+}
+
+function handleEditTitleCard() {
+
+}
+
+export const PopupCard = () => {
+    return <div></div>;
+}
