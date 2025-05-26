485a759a923fbfc589514e084a72f28ee7625614
Andorfii <andorfi.leon@gmail.com>
2025-05-26 15:25:49 +0100

    Update Django Settings

--- backend/miaonance_backend/settings.py
+++ backend/miaonance_backend/settings.py
@@ -9,8 +9,15 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
+from dotenv import load_dotenv
 from pathlib import Path
+
+# Secret Keys
+load_dotenv()
+
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
