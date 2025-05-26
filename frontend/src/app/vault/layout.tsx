1cca413ec1f60c65c96d5f92e5f61aed9037a4b3
dev-j03l <jojanj@tcd.ie>
2025-05-26 07:10:04 +0100

    Modified the layout to fix footer positioning

--- frontend/src/app/vault/layout.tsx
+++ frontend/src/app/vault/layout.tsx
@@ -4,9 +4,11 @@ import Header from "@/components/Header";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-    <Header/>
-    {children}
-    <Footer/>
+    <div className="flex flex-col min-h-screen">
+        <Header />
+        <main className="flex-1">{children}</main>
+        <Footer />
+    </div>
     </>
   );
 }
