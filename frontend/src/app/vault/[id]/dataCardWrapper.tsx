eac00f8117ddd0ce761d22f48e5d74e6e8f264d8
Zicheng Liang <liangzicheng02@gmail.com>
2025-05-31 18:44:59 +0800

    Further styling needed for the cards

--- frontend/src/app/vault/[id]/dataCardWrapper.tsx
+++ frontend/src/app/vault/[id]/dataCardWrapper.tsx
@@ -8,30 +8,32 @@ interface DataCardWrapperProps {
 }
 
 export default function DataCardWrapper({ children }: DataCardWrapperProps) {
-  const sampleArr = range(1, 5);
+  const sampleArr = range(1, 4);
+
   const cardCount = children ? Children.count(children) : sampleArr.length;
   console.log(`card count: `, cardCount);
   const numCols = cardCount <= 2 ? 1 : 2;
   console.log(`#cols`, numCols);
   const numRows = cardCount === 2 ? 2 : Math.ceil(cardCount / 2);
   console.log(`#rows`, numRows);
-  const lastItem = (num: number, cardCount: number) =>
-    num === cardCount && cardCount % 2 != 0;
+  const lastOddItem = (num: number, cardCount: number) =>
+    num === cardCount && cardCount % 2 === 1;
+
   return (
-    <div className="w-full gap-y-4">
+    <div className="w-full">
       <br />
       <header className={`h-[5vh] border rounded-lg p-2 text-center`}>
         <p className="font-bold">The control bar</p>
       </header>
       <br />
       <div
-        className={`h-[75vh] auto-rows-fr grid grid-cols-${numCols} grid-rows-${numRows} flex justify-between gap-4 overflow-auto`}
+        className={`h-[75vh] w-full grid grid-cols-${numCols} grid-rows-${numRows} flex gap-4`}
       >
         {sampleArr.map((num) => (
           <DataCard
             key={randomUUID()}
             id={num}
-            className={lastItem(num, cardCount) ? `col-span-2` : ``}
+            className={lastOddItem(num, cardCount) ? `col-span-2` : ``}
           />
         ))}
       </div>
